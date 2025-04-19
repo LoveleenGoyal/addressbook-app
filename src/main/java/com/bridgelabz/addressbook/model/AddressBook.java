5e3a059a27e2bf26c5ad584801bafb4c434dc87d
LoveleenGoyal <loveleengoyal26@gmail.com>
2025-04-19 15:31:18 +0530

    [Loveleen] ADD: User Address book model

--- src/main/java/com/bridgelabz/addressbook/model/AddressBook.java
+++ src/main/java/com/bridgelabz/addressbook/model/AddressBook.java
@@ -1,23 +1,41 @@
 package com.bridgelabz.addressbook.model;
 
+import com.bridgelabz.addressbook.dto.AddressBookDTO;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.util.List;
 
 @Entity
-@Data
 @NoArgsConstructor
-public class AddressBook {
+@Table(name = "user_address_data")
+public @Data class AddressBook {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    @Column(name = "user_id")
+    private int id;
 
     private String name;
     private String city;
     private String state;
     private String zipCode;
 
+    @ElementCollection
+    @CollectionTable(name="user_phone", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "phone_number")
     private List<String> phoneNumber;
 
+    @ElementCollection
+    @CollectionTable(name="user_email", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "email")
+    private List<String> email;
+
+    public AddressBook(AddressBookDTO addressBookDTO) {
+        this.name = addressBookDTO.getName();
+        this.city = addressBookDTO.getCity();
+        this.state = addressBookDTO.getState();
+        this.zipCode = addressBookDTO.getZipCode();
+        this.phoneNumber = addressBookDTO.getPhoneNumber();
+        this.email = addressBookDTO.getEmail();
+    }
 }
