6eadf757a96ad56864df36a4369bf4171bfd6c8d
LoveleenGoyal <loveleengoyal26@gmail.com>
2025-04-19 15:33:26 +0530

    [Loveleen] ADD: Adding properties to Address book DTO

--- src/main/java/com/bridgelabz/addressbook/dto/AddressBookDTO.java
+++ src/main/java/com/bridgelabz/addressbook/dto/AddressBookDTO.java
@@ -1,4 +1,20 @@
 package com.bridgelabz.addressbook.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AddressBookDTO {
+    private String name;
+    private String city;
+    private String state;
+    private String zipCode;
+
+    private List<String> phoneNumber;
+    private List<String> email;
 }
